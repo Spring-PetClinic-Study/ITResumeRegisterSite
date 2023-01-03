9577d493f77d8282a4a5b8021b04c9ff2f8107bc
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-01-03 22:49:45 +0900

    service -> 회원가입 기능 구현

--- src/main/java/kr/co/itresumeregistersite/service/UserService.java
+++ src/main/java/kr/co/itresumeregistersite/service/UserService.java
@@ -1,4 +1,32 @@
 package kr.co.itresumeregistersite.service;
 
+import kr.co.itresumeregistersite.domain.dto.userDto.SignUpDto;
+import kr.co.itresumeregistersite.domain.entity.User;
+import kr.co.itresumeregistersite.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class UserService {
+    private UserRepository userRepository;
+
+    // TODO 회원가입
+    public void signUp(SignUpDto signUpDto) {
+        User user = User.builder()
+                .identity(signUpDto.getIdentity())
+                .password(signUpDto.getPassword())
+                .name(signUpDto.getName())
+                .phone(signUpDto.getPhone())
+                .email(signUpDto.getEmail())
+                .birth(signUpDto.getBirth())
+                .address(signUpDto.getAddress())
+                .gender(signUpDto.getGender())
+                .build();
+
+        userRepository.save(user);
+    }
 }
