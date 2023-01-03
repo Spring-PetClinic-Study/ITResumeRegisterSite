45b4c2b11814aad7eecaaab8d6a1e96b33fc5828
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-01-03 22:45:27 +0900

    controller -> 회원가입 기능 구현

--- src/main/java/kr/co/itresumeregistersite/controller/UserController.java
+++ src/main/java/kr/co/itresumeregistersite/controller/UserController.java
@@ -1,4 +1,26 @@
 package kr.co.itresumeregistersite.controller;
 
+import kr.co.itresumeregistersite.domain.dto.userDto.SignUpDto;
+import kr.co.itresumeregistersite.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
 public class UserController {
+    private UserService userService;
+
+    // TODO 회원가입
+    @PostMapping("/user/signUp")
+    public void signUp(@RequestBody SignUpDto signUpDto) {
+        userService.signUp(signUpDto);
+    }
+
+    // TODO 로그인
+
+    // TODO 회원정보 조회
+
+    // TODO 회원정보 수정
+
+    // TODO 회원탈퇴
 }
