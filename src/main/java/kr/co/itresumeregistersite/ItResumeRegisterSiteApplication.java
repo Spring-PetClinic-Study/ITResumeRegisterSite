920a3161730d8a5e50964f724ed38281802f96bf
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-01-03 22:34:23 +0900

    first commit

---
+++ src/main/java/kr/co/itresumeregistersite/ItResumeRegisterSiteApplication.java
@@ -0,0 +1,13 @@
+package kr.co.itresumeregistersite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItResumeRegisterSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItResumeRegisterSiteApplication.class, args);
+    }
+
+}
---
+++ src/main/java/kr/co/itresumeregistersite/domain/entity/User.java
@@ -0,0 +1,14 @@
+package kr.co.itresumeregistersite.domain.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table
+public class User {
+}
