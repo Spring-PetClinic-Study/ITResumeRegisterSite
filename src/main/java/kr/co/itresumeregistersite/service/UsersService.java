00e0c7894a60814d612a7da23be7d04bdead8dc2
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-01-12 05:03:36 +0900

    service -> updatePassword 기능 및 예외 처리 구현

--- src/main/java/kr/co/itresumeregistersite/service/UsersService.java
+++ src/main/java/kr/co/itresumeregistersite/service/UsersService.java
@@ -6,12 +6,9 @@ import kr.co.itresumeregistersite.domain.exception.UsersException;
 import kr.co.itresumeregistersite.domain.exception.UsersExceptionType;
 import kr.co.itresumeregistersite.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.security.SecurityUtil;
-import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.security.Security;
 import java.util.Optional;
 
 @Service
@@ -20,7 +17,8 @@ public class UsersService {
     private UsersRepository usersRepository;
 
 
-    // TODO Spring Security(PasswordEncoder)에 대해 공부
+    // TODO Spring Security(PasswordEncoder)에 대해 공부 -> 개어려움
+    // TODO UsersServiceImpl implements UsersService 하는 이유에 대해 공부
     // 회원가입
     @Transactional
     public void signUp(SignUpDto signUpDto) throws Exception{
@@ -43,11 +41,15 @@ public class UsersService {
         usersRepository.save(users);
     }
 
-
+//    회원 로그인
+//    아이디, 비밀번호가 틀렸을 경우 예외 처리
+//    public void signIn(SignInDto signInDto) throws Exception {
+//
+//    }
 
     // TODO 일치하는 회원정보가 없을 경우 예외 처리
     // 회원정보 조회
-    public ReadDto getUser(String identity) throws Exception {
+    public UsersInfoDto getUser(String identity) throws Exception {
         Users users = usersRepository.findByIdentity(identity)
                 .orElseThrow(() -> new UsersException(UsersExceptionType.NOT_FOUND_USERS));
 
@@ -57,38 +59,28 @@ public class UsersService {
     // 회원정보 수정
     @Transactional
     public void updateUser(UsersUpdateDto usersUpdateDto) throws Exception {
-        Optional<Users> users = usersRepository.findByIdentity(usersUpdateDto.getIdentity());
+        Users users = usersRepository.findByIdentity(usersUpdateDto.getIdentity())
+                .orElseThrow(() -> new UsersException(UsersExceptionType.NOT_FOUND_USERS));
 
-        // TODO 하나로 묶을 수 있는 방법(update 메소드로 전부 다 수정이 가능하도록 할지 아님 지금처럼 할지)
-        users.get().updateEmail(usersUpdateDto.getEmail());
-        users.get().updatePhone(usersUpdateDto.getPhone());
-        users.get().updateAddress(usersUpdateDto.getAddress());
 
-        if (usersUpdateDto.getEmail().isEmpty()) {
-            throw new UsersException(UsersExceptionType.NOT_EXIST_EMAIL);
-        }
-        else if (usersUpdateDto.getPhone().isEmpty()) {
-            throw new UsersException(UsersExceptionType.NOT_EXIST_PHONE);
-        }
-        else if (usersUpdateDto.getAddress().isEmpty()) {
-            throw new UsersException(UsersExceptionType.NOT_EXIST_ADDRESS);
-        }
-
-        usersRepository.save(users.get());
+//        usersUpdateDto.email().ifPresent(users::updateEmail);
+//        usersUpdateDto.phone().ifPresent(users::updatePhone);
+//        usersUpdateDto.address().ifPresent(users::updateAddress);
     }
 
-    // TODO 입력한 비밀번호가 틀렸을 경우 예외 처리
+    // TODO 수정 전 비밀번호와 수정 후 비밀번호가 같을 경우 예외 처리
     // 회원 비밀번호 수정
     @Transactional
     public void updatePassword(UsersPasswordDto usersPasswordDto) throws Exception {
         Users users = usersRepository.findByIdentity(usersPasswordDto.getIdentity())
                 .orElseThrow(() -> new UsersException(UsersExceptionType.NOT_FOUND_USERS));
 
+
         if (usersPasswordDto.getPassword().equals(usersPasswordDto.getChangePassword())) {
             throw new UsersException(UsersExceptionType.WRONG_PASSWORD);
         }
 
-        usersRepository.save(users);
+        users.updatePassword(usersPasswordDto.getPassword());
     }
 
     // 회원탈퇴
