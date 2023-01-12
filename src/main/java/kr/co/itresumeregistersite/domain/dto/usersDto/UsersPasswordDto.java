9450150a90f0a4db03244ba833d83dbccdf9db10
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-01-12 18:35:13 +0900

    password 암호화를 위해 Security에 대해 공부 -> BCrytPasswordEncoder()

--- src/main/java/kr/co/itresumeregistersite/domain/dto/usersDto/UsersPasswordDto.java
+++ src/main/java/kr/co/itresumeregistersite/domain/dto/usersDto/UsersPasswordDto.java
@@ -3,7 +3,7 @@ package kr.co.itresumeregistersite.domain.dto.usersDto;
 import lombok.Getter;
 
 @Getter
-public class UsersPasswordDto {
+public class UsersPasswordDto { // 회원 비밀번호 수정 Dto
 
     private String identity;
 
--- src/main/java/kr/co/itresumeregistersite/domain/entity/Users.java
+++ src/main/java/kr/co/itresumeregistersite/domain/entity/Users.java
@@ -15,34 +15,34 @@ public class Users {
 
     @Id @GeneratedValue
     @Column(name = "users_id")
-    private Long usersId;
+    private Long usersId;   // primary Key
 
     @Column(length = 20, nullable = false, unique = true)
-    private String identity;
+    private String identity;    // 아이디
 
     @Column(length = 20, nullable = false)
-    private String password;
+    private String password;    // 비밀번호
 
     @Column(length = 20, nullable = false)
-    private String checkPassword;
+    private String checkPassword;   // 비밀번호 확인
 
     @Column(length = 20, nullable = false)
-    private String name;
+    private String name;    // 이름
 
     @Column(length = 20, nullable = false, unique = true)
-    private String phone;
+    private String phone;   // 전화번호
 
     @Column(length = 30, nullable = false, unique = true)
-    private String email;
+    private String email;   // 이메일
 
     @Column(length = 20, nullable = false)
-    private String birth;
+    private String birth;   // 생일
 
     @Column(length = 30, nullable = false)
-    private String address;
+    private String address; // 주소
 
     @Column(length = 10, nullable = false)
-    private String gender;
+    private String gender;  // 성별
 
 
 
@@ -60,8 +60,8 @@ public class Users {
         this.address = address;
     }
 
-//    비밀번호 변경, 회원탈퇴 시 비밀번호를 확인하며, 이때 비밀번호의 일치여부를 판단
-//    public boolean matchPassword(PasswordEncoder passwordEncoder, String checkPassword) {
-//        return passwordEncoder.matches(checkPassword, getPassword());
-//    }
+    // 비밀번호 변경, 회원탈퇴 시 비밀번호를 확인하며, 이때 비밀번호의 일치여부를 판단
+    public boolean matchPassword(PasswordEncoder passwordEncoder, String checkPassword) {
+        return passwordEncoder.matches(checkPassword, getPassword());
+    }
 }
