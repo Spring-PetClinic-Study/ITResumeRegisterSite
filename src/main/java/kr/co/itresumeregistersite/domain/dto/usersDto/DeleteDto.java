2d0ec9c8bb59bbdcd2b9c2f801331d2004ab06bd
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-01-12 18:36:13 +0900

    getUser -> userInfo로 클래스명 수정 각 entity 주석처리

--- src/main/java/kr/co/itresumeregistersite/domain/dto/usersDto/DeleteDto.java
+++ src/main/java/kr/co/itresumeregistersite/domain/dto/usersDto/DeleteDto.java
@@ -3,7 +3,7 @@ package kr.co.itresumeregistersite.domain.dto.usersDto;
 import lombok.Getter;
 
 @Getter
-public class DeleteDto {
+public class DeleteDto {    // 회원탈퇴 Dto
 
     private String identity;
 
---
+++ src/main/java/kr/co/itresumeregistersite/domain/dto/usersDto/SignInDto.java
@@ -0,0 +1,11 @@
+package kr.co.itresumeregistersite.domain.dto.usersDto;
+
+import lombok.Getter;
+
+@Getter
+public class SignInDto {
+
+    private String identity;
+
+    private String password;
+}
