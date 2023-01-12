0f170ea5b63e083eb6a9cc6561b6ca81430303a8
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-01-12 18:36:34 +0900

    exception 기능 구현 수정

--- src/main/java/kr/co/itresumeregistersite/domain/exception/UsersExceptionType.java
+++ src/main/java/kr/co/itresumeregistersite/domain/exception/UsersExceptionType.java
@@ -10,20 +10,9 @@ public enum UsersExceptionType implements BaseExceptionType {
     // 회원가입, 로그인을 할 경우
     ALREADY_EXIST_USERSIDENTITY(600, HttpStatus.OK, "이미 존재하는 아이디입니다!"),
 
-    WRONG_IDENTITY(601, HttpStatus.OK, "아이디가 잘못되었습니다!"),
-
     WRONG_PASSWORD(602, HttpStatus.OK, "비밀번호가 잘못되었습니다!"),
 
-    NOT_FOUND_USERS(603, HttpStatus.OK, "회원정보가 없습니다!"),
-
-
-
-    NOT_EXIST_EMAIL(604, HttpStatus.OK, "이메일을 입력해주세요!"),
-
-    NOT_EXIST_PHONE(605, HttpStatus.OK, "전화번호를 입력해주세요!"),
-
-    NOT_EXIST_ADDRESS(606, HttpStatus.OK, "주소를 입력해주세요!");
-
+    NOT_FOUND_USERS(603, HttpStatus.OK, "회원정보가 없습니다!");
 
     private int errorCode;
     private HttpStatus httpStatus;
