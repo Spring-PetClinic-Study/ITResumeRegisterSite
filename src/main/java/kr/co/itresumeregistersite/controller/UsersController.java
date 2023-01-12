c9112a047e55b1080c2f83323aa6afec787119a4
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-01-13 00:04:47 +0900

    fix: Controller update

--- src/main/java/kr/co/itresumeregistersite/controller/UsersController.java
+++ src/main/java/kr/co/itresumeregistersite/controller/UsersController.java
@@ -14,31 +14,31 @@ public class UsersController {
     // TODO URL 및 @Mapping 등 Http Method에 대한 공부
 
     // 회원가입
-    @PostMapping("/signUp")
+    @PostMapping
     public void signUp(@RequestBody SignUpDto signUpDto) throws Exception {
         usersService.signUp(signUpDto);
     }
 
     // 회원정보 조회
-    @GetMapping("")
+    @GetMapping
     public UsersInfoDto userInfo(Long userId) throws Exception {
         return usersService.userInfo(userId);
     }
 
     // 회원정보 수정
-    @PutMapping("")
+    @PutMapping
     public void updateUser(UsersUpdateDto usersUpdateDto) throws Exception {
         usersService.updateUser(usersUpdateDto);
     }
 
-    // 회원 비밀번호 수정
-    @PutMapping("")
+    //TODO -> URL이 왜 겹치면 안뙤는지 이해하기
+    @PutMapping("/password")
     public void updatePassword(UsersPasswordDto usersPasswordDto) throws Exception {
         usersService.updatePassword(usersPasswordDto);
     }
 
     // 회원탈퇴
-    @DeleteMapping("")
+    @DeleteMapping
     public void delete(DeleteDto deleteDto) throws Exception {
         usersService.delete(deleteDto);
     }
