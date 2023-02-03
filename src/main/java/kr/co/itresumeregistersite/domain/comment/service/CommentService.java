19f321f3c0a2cb5ac5ba0121197a8cf118140211
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-02-03 16:56:10 +0900

    CommentService @Transactional 추가

--- src/main/java/kr/co/itresumeregistersite/domain/comment/service/CommentService.java
+++ src/main/java/kr/co/itresumeregistersite/domain/comment/service/CommentService.java
@@ -8,6 +8,7 @@ import kr.co.itresumeregistersite.global.error.exception.board.NotExistWriterExc
 import kr.co.itresumeregistersite.global.error.exception.comment.InvalidCommentException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +20,7 @@ public class CommentService {
     private final CommentRepository commentRepository;
 
     // 댓글 등록
+    @Transactional
     public void registerComment(RegisterCommentDto registerCommentDto) {
         // 댓글 작성자와 내용이 없을 경우 예외처리
         writerHasNotBeenEntered(registerCommentDto.getWriter());
@@ -29,11 +31,13 @@ public class CommentService {
     }
 
     // 댓글 조회
+    @Transactional(readOnly = true)
     public List<Comment> findAllComment() {
         return commentRepository.findAll();
     }
 
     // 댓글 수정
+    @Transactional
     public void editComment(EditCommentDto editCommentDto) {
         // 작성된 댓글들이 없을 경우 예외처리
         noCommentHaveBeenWritten(editCommentDto.getCommentId());
@@ -52,6 +56,7 @@ public class CommentService {
     }
 
     // 댓글 삭제
+    @Transactional
     public void deleteComment(Long commentId) {
         // 작성한 댓글이 없을 경우 예외처리
         noCommentHaveBeenWritten(commentId);
